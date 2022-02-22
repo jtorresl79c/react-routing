5669fb5df6946ba97aa474cea35352dd94fef235
Jason Torres <jtorresl79c@gmail.com>
2022-02-22 13:16:45 -0800

    8. - Route parameters - Falto explicar el porque es importante enviar los props

--- src/components/productDetails.jsx
+++ src/components/productDetails.jsx
@@ -8,6 +8,9 @@ class ProductDetails extends Component {
   render() {
     return (
       <div>
+        {/* En App.js en la parte en donde dice '{...props}' se explica que se pone esto porque de lo contrario no se envian 3 props (porque se usa el atributo render en vez del atributo
+        component), uno de esos props que al no poner '{...props}' no se envian es el props 'match', enviar este prop es importante, ya que este prop es el que contiene los atributos
+        en la url y que como en este caso podemos necesitar: */}
         <h1>Product Details - {this.props.match.params.id}</h1>
         <button onClick={this.handleSave}>Save</button>
       </div>
