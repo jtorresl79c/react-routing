f7aed14464de1ffd1071a7c713f219aa799c0a7a
Jason Torres Luis <jtorresl79c@gmail.com>
2022-02-27 18:04:51 -0800

    10. Query String Parameters

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import queryString from 'query-string'
 
 // const Posts = (props) => { -- P23M@# --
-const Posts = ({ match }) => {  // Recuerda como funciona la destructuracion, usando esa logica podemos poner { propertie } 
+const Posts = ({ match, location }) => {  // Recuerda como funciona la destructuracion, usando esa logica podemos poner { propertie } 
   // para asi evitar poner 'props' -- P23M@# -- como el caso de arriba
   // const year = props.match.params.year
   // const month = props.match.params.month
   const year = match.params.year
   const month = match.params.month
+  
+  // location.search retorna la url actual
+  const resultQueryString = queryString.parse(location.search) // Retorna un object en el cual sus propiedades son los query string de la url actual
+  const sortBy = resultQueryString.sortBy // Retorna string
+  const approved = resultQueryString.approved // Retorna string
+  console.log(approved, sortBy) // Al retornar string si queremos usar esto para hacer validaciones por ejemplo hay que castear las variables y convertirlas
+  // en el caso de 'approved' a boolean
   return (
     <div>
       <h1>Posts</h1>
