668a47754132d44d3542d8b9b3762cdc33ded08e
Jason Torres Luis <jtorresl79c@gmail.com>
2022-02-27 19:49:01 -0800

    12. Programmatic Navigation - Todo el codigo de este ejemplo se ve en     productDetails.jsx

--- src/components/productDetails.jsx
+++ src/components/productDetails.jsx
@@ -3,6 +3,15 @@ import React, { Component } from "react";
 class ProductDetails extends Component {
   handleSave = () => {
     // Navigate to /products
+    // this.props.history.push('/products') // Esta forma guarda el historial al navegar al 
+    // path que le estamos indicando, por lo tanto podemos usar el boton de atras
+
+
+    this.props.history.replace('/products') // En cambio este tambien nos mueve a la ruta
+    // '/products' con la diferencia que este no guarda el historial, por lo que no es
+    // posible usar el boton de atras, porque no se guarda historial, comentan que este es util
+    // cuando se aplasta el boton de Login, ya que al presionar el boton de atras no queremos
+    // que lo manden de nuevo a la pagina del login.
   };
 
   render() {
